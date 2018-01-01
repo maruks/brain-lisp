dcde3ff6d9ddb0419e591b11cbca63535a302061
Maris Orbidans <MarisOrbidans@thisisnoble.com>
2018-01-01 05:34:10 +0000

    compress image

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 buildapp --output brain --manifest-file quicklisp-manifest.txt \
+    --compress-core \
     --load-system brain \
     --eval '(defun main (args) (declare (ignore args)) (brain:start) (sb-impl::toplevel-repl nil))' \
     --entry main
