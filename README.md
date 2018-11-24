21ea7038e49cd4493dfe12fa4997097ec7e3298f
Maris Orbidans <maris.orbidans@onthemarket.com>
2018-11-24 23:04:25 +0000

    update README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Brain cellular automaton
+
+https://en.wikipedia.org/wiki/Brian%27s_Brain
+
+## Install dependencies
+
+    (ql:quickload '(:websocket-driver :clack :cl-json :hunchentoot))
+
+## Start
+
+    sbcl --eval "(progn (asdf:operate 'asdf:load-op 'brain) (brain:start))"
+
+## Create manifest file
+
+    (asdf:operate 'asdf:load-op 'brain)
+    (ql:write-asdf-manifest-file "quicklisp-manifest.txt")
+
+## Build executable
+
+    buildapp --output brain --manifest-file quicklisp-manifest.txt \
+        --compress-core \
+        --load-system brain \
+        --eval '(defun main (args) (declare (ignore args)) (brain:start) (sb-impl::toplevel-repl nil))' \
+        --entry main
--- brain.lisp
+++ brain.lisp
@@ -27,15 +27,18 @@
      (declare (ignore env))
      '(200 nil nil))))
 
+(defun initialize-grid (grid width height)
+  (loop
+     for x from 0 to (1- width)
+     do (loop
+	   for y from 0 to (1- height)
+	   do (when (zerop (random 4))
+		(setf (gethash (cons x y) grid) (if (zerop (random 2)) :on :dying)))))
+  grid)
+
 (defun new-grid (width height)
   (let ((grid (make-hash-table :size (* width height) :test #'equal)))
-    (loop
-       for x from 0 to (1- width)
-       do (loop
-	     for y from 0 to (1- height)
-	     do (when (zerop (random 4))
-		  (setf (gethash (cons x y) grid) (if (zerop (random 2)) :on :dying)))))
-    grid))
+    (initialize-grid grid width height)))
 
 (defun next-state (grid point)
   (let ((state (gethash point grid :off)))
@@ -45,16 +48,18 @@
       (:off (if (eq 2 (count-live-neighbours grid (car point) (cdr point))) :on :off)))))
 
 (defun next-grid (grid width height)
-  (let* ((points (apply #'append (loop for x from 0 to (1- width)
-				    collect (loop for y from 0 to (1- height)
-					       collect (cons x y)))))
-	 (states (mapcar (lambda (p) (next-state grid p)) points)))
-    (clrhash grid)
-    (loop for p in points
-       for s in states
-       do (unless (eq :off s)
-	    (setf (gethash p grid) s)))
-    grid))
+  (if (zerop (hash-table-count grid))
+      (initialize-grid grid width height)
+      (let* ((points (apply #'append (loop for x from 0 to (1- width)
+					collect (loop for y from 0 to (1- height)
+						   collect (cons x y)))))
+	     (states (mapcar (lambda (p) (next-state grid p)) points)))
+	(clrhash grid)
+	(loop for p in points
+	   for s in states
+	   do (unless (eq :off s)
+		(setf (gethash p grid) s)))
+	grid)))
 
 (defun grid-to-list (grid)
   (let ((cells (loop for point being the hash-keys of grid
@@ -86,6 +91,7 @@
 		  (send ws (encode-json-alist-to-string (colors-msg)))))
 	(:next (let* ((state (afield :state client))
 		      (grid (afield :grid client)))
+
 		 (case state
 		   (:started (progn (send ws (encode-json-alist-to-string (grid-to-list grid)))
 				    (next-grid grid (afield :width client) (afield :height client))))
