ee5511bc374efa776e671e09c0bc6200f4d1fa75
Maris Orbidans <maris.orbidans@onthemarket.com>
2019-04-14 00:38:03 +0100

    remove buildapp script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-buildapp --output brain --manifest-file quicklisp-manifest.txt \
-    --compress-core \
-    --load-system brain \
-    --eval '(defun main (args) (declare (ignore args)) (brain:start) (sb-impl::toplevel-repl nil))' \
-    --entry main
